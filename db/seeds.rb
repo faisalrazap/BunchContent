4fec84b3a1f3289786c04f4dee4730da80c88462
Bilal Khan <baberjaved@gmail.com>
2015-11-29 00:14:11 +0500

    User: Added seed to create new user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+puts 'Creating User'
+User.create(email: "admin@contentbunch.com", password: "password")
