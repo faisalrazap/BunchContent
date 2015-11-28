af9df57028b59cbbf4a6f7049df5aee96f0a7d04
Bilal Khan <bilalkhaan@hotmail.com>
2015-11-29 00:53:14 +0500

    User: Updated the alert message

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,7 @@
 class RegistrationsController < Devise::RegistrationsController
 
   def new
-    redirect_to new_user_session_path, alert: 'You are not allowed to create new account'
+    redirect_to new_user_session_path, alert: 'You are not allowed to sign up'
   end
 
 end
