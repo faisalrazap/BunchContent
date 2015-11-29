2811a628c1f6c1e61c6aa97873d129b3317af99f
Bilal Khan <bilalkhaan@hotmail.com>
2015-11-29 10:41:53 +0500

    Changes incorporated

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -4,4 +4,8 @@ class RegistrationsController < Devise::RegistrationsController
     redirect_to new_user_session_path, alert: 'You are not allowed to sign up'
   end
 
+  def create
+    redirect_to new_user_session_path, alert: 'You are not allowed to sign up'
+  end
+
 end
